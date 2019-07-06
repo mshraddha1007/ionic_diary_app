c5d7f700dcab24f5bfe4b49827f9e9a814c85f0a
mshraddha1007 <mshraddha1007@gmail.com>
2019-07-06 17:19:25 +0530

    'firstCommit'

---
+++ src/app/services/item.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  items: Array<any> = [
+    {
+      'id': "1",
+      'title': "20 july 2016 ",
+      'description': "Hey Diary"
+    }
+  ]
+
+  constructor() { }
+
+  createItem(title, description){
+
+    let randomId = Math.random().toString(36).substr(2, 5);
+
+    this.items.push({
+      'id': randomId,
+      'title': title,
+      'description': description
+    });
+  }
+
+  getItems(){
+    return this.items;
+  }
+
+  getItemById(id){
+    return this.items.filter(item => item.id === id);
+  }
+
+  updateItem(newValues){
+    let itemIndex = this.items.findIndex(item => item.id == newValues.id);
+    this.items[itemIndex] = newValues;
+  
+  }
+
+  deleteItem(deleteValue){
+    let itemIndex = this.items.findIndex(item => item.id == deleteValue.id);
+    this.items.splice(itemIndex);
+  }
+}
